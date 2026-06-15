a5cde2d3b57c5e3b1807d9b75dbaa68e37fe77d8
shwetha susan gijo <shwethasusangijo@gmail.com>
2026-06-15 12:14:26 +0530

    button motion 2

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
  let bott = document.getElementsByTagName("button")
         let mov=10
+        let predict=document.querySelector(".predict")
         bott[0].addEventListener("mouseover",function(){
             bott[0].style.marginLeft =Math.floor(Math.random()*mov)+"rem"
             bott[0].style.marginTop =Math.floor(Math.random()*mov)+"rem"
             
         })
+
+
+        setTimeout(function(){
+            bott[0].style.display="none"
+            predict.innerText="Sorry...."
+
+        },5000)
